10bf1c950726c27799f120a0a973a2cc1c68c3e4
tallisazevedo <tallisazevedo@gmail.com>
2025-10-05 23:23:04 -0300

    Organização de Pastas

--- API/ScreenSound/Program.cs
+++ API/ScreenSound/Program.cs
@@ -1,4 +1,18 @@
-﻿Musica musica1 = new Musica();
-musica1.nome = "Roxane";
-musica1.dataLancamento = DateTime.Parse("12/12/2008");
+﻿using System.Text.Json;
+using ScreenSound.Modelos;
 
+using (HttpClient cliente = new HttpClient())
+{
+    try
+    {
+        string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        Console.WriteLine($"{musicas}");
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problemas: {ex.Message}");
+    }
+}
