4e8473a6d6c959a2a54af2fb86da5734fd3ecf78
Tallis Azevedo <124191302+tallisazevedo@users.noreply.github.com>
2025-10-05 22:33:56 -0300

    Add files via upload

---
+++ API/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using ScreenSound.Modelos;
+
+using (HttpClient cliente = new HttpClient())
+{
+    try
+    {
+        string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musica = JsonSerializer.Deserialize<List<Musica>>(resposta);
+
+
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problemas: {ex.Message}");
+    }
+}
